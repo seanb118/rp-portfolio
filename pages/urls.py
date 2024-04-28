a9152660e64802fbd7b7ad2f9be76a45664e7851
Xiang Bai <xbai@billiontoone.com>
2024-04-28 10:48:24 -0700

    add images to projects; route projects from home view

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,10 @@
 
 from django.urls import path
 from pages import views
+from projects import views as projects_views
 
 urlpatterns = [
     path("", views.home, name='home'),
+    path("projects/", projects_views.project_index, name="projects"),
 ]
+
--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+# projects/admin.py
+
+from django.contrib import admin
+from projects.models import Project
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Project, ProjectAdmin)
